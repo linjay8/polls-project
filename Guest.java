2a45929aba76e8e6b0c1592bfbe56b185c1b6110
Jay Lin <linjay@usc.edu>
2020-11-16 21:10:32 -0800

    basic class structures

---
+++ Guest.java
@@ -0,0 +1,16 @@
+public class Guest extends User
+{
+
+	public Guest(String name_, String email_, int userId_)
+	{
+		super( name_,  email_,  0,  userId_);
+	}
+
+	@Override
+	public String getFullName()
+	{
+		return "Anonymous";
+	}
+
+
+}

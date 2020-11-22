ad655799b32ab4f78510054e974e5cc84038a0b4
qooqpoop <vmai@usc.edu>
2020-11-22 04:31:13 -0800

    Finishing chat.

--- src/chat/availableChatrooms.java
+++ src/chat/availableChatrooms.java
@@ -27,16 +27,17 @@ public class availableChatrooms extends HttpServlet {
 	public int senderId;
 	public String recipientId;
 
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response, String senderID)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
-		
-		//need to get senderId from requestdispatcher? or a button that sends value to this servlet (URl query get)
-		//senderId = DatabaseUtil.getUserId(request.getParameter("email")); //get senderId from email
-		 senderId = 1;
-		 //request.setAttribute("senderID", 1);
+	
+		//senderId = DatabaseUtil.getUserId(request.getSession().getAttribute("email")); //get senderId from email
+		//request.getSession().setAttribute("senderId", senderId);
+		//String temp = (String)request.getSession().getAttribute("email");
+		senderId =  DatabaseUtil.getUserId(senderID);
+		//request.setAttribute("senderID", 1);
 		 
-		 
+		//request.getSession().setAttribute("senderId", senderId);
 		PrintWriter out = response.getWriter();
 
 		try {
@@ -77,6 +78,7 @@ public class availableChatrooms extends HttpServlet {
 				}
 
 			}
+			
 			out.println("</form>");
 			out.println("</body>");
 			out.println("</html>");
@@ -92,28 +94,22 @@ public class availableChatrooms extends HttpServlet {
 			out.println("</body>");
 			out.println("</html>");
 			System.out.println(e);
-		} finally {
-			//do i still need to send senderId to the next page?
-			 //request.setAttribute("senderId", senderId);
-			// request.getParameter("recipient").toString());
-			
-			//RequestDispatcher dispatcher = request.getRequestDispatcher("/chatWindow");
-			//dispatcher.forward(request, response);
-			// request.getRequestDispatcher("/chatWindow").include(request, response);
-		}
+		} 
 
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		processRequest(request, response);
+		String temp = (String)request.getSession().getAttribute("email");
+		processRequest(request, response, temp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		processRequest(request, response);
+		String temp = (String)request.getSession().getAttribute("email");
+		processRequest(request, response, temp);
 	}
 	
 	HttpSession session;
